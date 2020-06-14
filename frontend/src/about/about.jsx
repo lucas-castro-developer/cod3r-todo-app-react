c8119a7bd7630ee0de6181e34ef12885f33013e5
lucas.castro@acct.global <lucas.sc98@hotmail.com>
2020-06-14 19:17:30 -0300

    Add todoApp structure of backend and frontend

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós"></PageHeader>
+        
+        <h2>Nossa História</h2>
+        <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae architecto pariatur eos iure molestiae animi nobis dolores possimus accusamus id, quis asperiores itaque? Fuga sapiente maiores aut amet ut quas?</p>
+
+        <h2>Missão e Visão</h2>
+        <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, expedita. Eligendi, sapiente, ipsa obcaecati culpa dolorum laudantium praesentium provident molestias debitis iste natus quidem minus repellendus! Voluptas aperiam optio totam.</p>
+
+        <h2>Imprensa</h2>
+        <p>"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio aut deleniti blanditiis fugit cumque. Facere, exercitationem sunt ut explicabo recusandae nobis aliquam doloribus ipsum necessitatibus soluta veritatis quidem, aperiam quaerat.</p>
+    </div>
+)
---
+++ frontend/src/index.jsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+ReactDOM.render(<App />, document.getElementById('app'))
