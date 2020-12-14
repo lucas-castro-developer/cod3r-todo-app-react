0d1ff1aec0ee484c9f0a7d2e2dcc8cab02492096
Lucas Castro <lucas.sc98@hotmail.com>
2020-12-14 20:46:51 -0300

    Icon button and grid components

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export default props => (
-    <div role="form" className="todoForm">
-        <div className="col-xs-12 col-sm-9 col-md-10">
-            <input id="description" className="form-control"
-                placeholder="Adicione uma tarefa"></input>
-        </div>
+import Grid from "../template/Grid";
+import IconButton from "../template/iconButton";
 
-        <div className="col-xs-12 col-sm-3 col-md-2">
-            <button className="btn btn-primary">
-                <i className="fa fa-plus"></i>
-            </button>
-        </div>
-    </div>
-)
+export default (props) => (
+  <div role="form" className="todoForm">
+    <Grid cols="12 9 10">
+      <input
+        id="description"
+        className="form-control"
+        placeholder="Adicione uma tarefa"
+      ></input>
+    </Grid>
+
+    <Grid cols="12 3 2">
+      <IconButton style="primary" icon="plus"></IconButton>
+    </Grid>
+  </div>
+);
