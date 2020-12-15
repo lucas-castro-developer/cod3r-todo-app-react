40518fd0769f95e383735c7982e3a4860d9d408c
Lucas Castro <lucas.sc98@hotmail.com>
2020-12-15 18:26:51 -0300

    Add event

--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -7,6 +7,5 @@ export default (props) => (
     <button className={"btn btn-" + props.style} onClick={props.onClick}>
       <i className={"fa fa-" + props.icon}></i>
     </button>
-    ;
   </If>
 );
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,16 +1,25 @@
-import React, { Component }  from 'react'
-import PageHeader from '../template/pageHeader'
-import TodoForm from './todoForm'
-import TodoList from './todoList'
+import React, { Component } from "react";
+import PageHeader from "../template/pageHeader";
+import TodoForm from "./todoForm";
+import TodoList from "./todoList";
 
 export default class Todo extends Component {
-    render() {
-        return (
-            <div>
-                <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm />
-                <TodoList />
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+        <TodoForm handleAdd={this.handleAdd} />
+        <TodoList />
+      </div>
+    );
+  }
 }
