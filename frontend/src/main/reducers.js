16c7a45f7d9c1a8ae688a9621251f01ea3e5c1d6
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-21 17:45:57 -0300

    Setting up the reducers

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,25 @@
+import { combineReducers } from "redux";
+
+const rootReducer = {
+  todo: () => ({
+    description: "Ler livro",
+    list:
+      ({
+        _id: 1,
+        description: "Pagar fatura do cartão",
+        done: true,
+      },
+      {
+        _id: 2,
+        description: "Reunião com a equipe às 10:00",
+        done: false,
+      },
+      {
+        _id: 3,
+        description: "Consultas médicas na terça depois do almoço",
+        done: false,
+      }),
+  }),
+};
+
+export default rootReducer
